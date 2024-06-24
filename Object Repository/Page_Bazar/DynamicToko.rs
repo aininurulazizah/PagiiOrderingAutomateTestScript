58e564bc6933b985f12c067cd9668ce1e64f340b
aininurulazizah <aini.nurul.tif421@polban.ac.id>
2024-06-24 16:31:21 +0700

    first commit

---
+++ Object Repository/Page_Bazar/DynamicToko.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>DynamicToko</name>
+   <tag></tag>
+   <elementGuidId>43e0af19-0c7b-4f51-a5c8-e60e31cf7f7a</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[contains(text(), '${GlobalVariable.Toko}')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/Page_Bazar/h3_AiSpice Cafe.rs
@@ -0,0 +1,123 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>h3_AiSpice Cafe</name>
+   <tag></tag>
+   <elementGuidId>3e136e77-ab4a-489c-b17a-a13ebbb42d62</elementGuidId>
+   <imagePath></imagePath>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='Search'])[1]/following::h3[1]</value>
+      </entry>
+      <entry>
+         <key>IMAGE</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[(text() = 'AiSpice Cafe' or . = 'AiSpice Cafe')]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:role=heading[name=&quot;AiSpice Cafe&quot;i]</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h3</value>
+      <webElementGuid>dcabd592-ba93-465f-a182-8c0c57e1d98c</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value> text-md font-[800] text-[#2E3840]</value>
+      <webElementGuid>212a2f6f-4860-4b82-91bb-95f31efec63c</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>AiSpice Cafe</value>
+      <webElementGuid>824ba4b8-7e65-4fb1-9af7-0eed77c2ed40</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[@class=&quot;__className_96f5ea max-w-md mx-auto min-w-[320px]&quot;]/div[@class=&quot;flex flex-col min-h-screen&quot;]/div[@class=&quot;flex flex-col flex-1 gap-5 px-4 mt-3&quot;]/div[@class=&quot;text-black mt-2 mb-10&quot;]/div[@class=&quot;mx-auto max-w-screen-xl&quot;]/div[@class=&quot;grid gap-x-2 gap-y-4 grid-cols-2 auto-col-max&quot;]/div[@class=&quot;cursor-pointer&quot;]/div[@class=&quot;w-full&quot;]/div[@class=&quot;w-full&quot;]/div[@class=&quot;flex justify-between rounded-b-[16px] border-1 flex-col relative border border-gray-100 bg-white p-3 gap-2 min-h-[110px]&quot;]/div[@class=&quot;min-h-[110px] max-h-[120px]&quot;]/h3[@class=&quot;text-md font-[800] text-[#2E3840]&quot;]</value>
+      <webElementGuid>8a346032-fa68-4a60-9427-a54ea38e461e</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Search'])[1]/following::h3[1]</value>
+      <webElementGuid>adabf6cf-31d6-43e0-8d98-93b0aaba6017</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Filter'])[1]/following::h3[1]</value>
+      <webElementGuid>bdc14e0f-a5c7-4688-be78-1a65cc6d544d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Baros'])[1]/preceding::h3[1]</value>
+      <webElementGuid>1bc0f119-18c7-4018-869b-173f078196f9</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='William Caffe'])[1]/preceding::h3[1]</value>
+      <webElementGuid>f882b18b-c362-460f-83ff-f5e4f127070b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='AiSpice Cafe']/parent::*</value>
+      <webElementGuid>766c20cc-bb92-4e08-b10e-4ed7bf28fa63</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//h3</value>
+      <webElementGuid>36a61f98-8520-4b9d-afd0-d736021ade3b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//h3[(text() = 'AiSpice Cafe' or . = 'AiSpice Cafe')]</value>
+      <webElementGuid>56f74520-8b4b-40c2-a8be-f147e462275e</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
